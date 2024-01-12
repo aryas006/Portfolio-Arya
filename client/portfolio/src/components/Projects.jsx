d5376586fe72f0fee996d1e06104d4ff3e212eea
Arya <128399498+aryas006@users.noreply.github.com>
2024-01-12 23:19:19 +0530

    commit

--- client/portfolio/src/components/Projects.jsx
+++ client/portfolio/src/components/Projects.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import { Element } from "react-scroll";
 import "./Project.css";
+import arrow from "./arrow.png";
 
 export default function Projects() {
   return (
     <Element name="container-projects">
       <section id="container-projects">
-        <div>Projects</div>
+        <div className="projects-main">
+          {" "}
+          <div className="project-names main">Projects</div>
+          <div className="project-names">
+            <a className="title"> EthTransferDAPP</a>
+
+            <img className="arrow" src={arrow} />
+          </div>
+          <div className="project-names">
+            <a className="title">BlogDAPP</a>{" "}
+            <img className="arrow" src={arrow} />
+          </div>
+          <div className="project-names">
+            <a className="title">ID-CardDApp</a>{" "}
+            <img className="arrow" src={arrow} />
+          </div>
+          <div className="project-names">
+            <a className="title">PRODO</a> <img className="arrow" src={arrow} />
+          </div>
+        </div>
       </section>
     </Element>
   );
